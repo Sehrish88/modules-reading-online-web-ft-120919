358f78cf4fd9b624f6ce1014d0660982ee29f89e
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-02 21:24:19 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,7 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid 
-  extend MetaDancing 
-  include Dance 
-  
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-  end 
-end 
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
